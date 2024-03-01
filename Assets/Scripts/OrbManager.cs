720a899207d0ff79296374e5f0da3bd0d8ad0421
Giles-MangoFarm <rhys.oscar101@gmail.com>
2024-03-01 19:47:25 +1100

    Core clicking & magic systems implemented

---
+++ Assets/Scripts/OrbManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class OrbManager : MonoBehaviour
+{
+    public Text insightText;
+    public Text insightPerSecText;
+    public Text autoClickText;
+    public Text clickUpgradeText;
+
+    public float insight;
+    public float insightAmount = 1f;
+    public float insightPerSec = 0f;
+
+    public float autoClickCost = 10f;
+    public float autoClickUpgradeLevel;
+
+    public float clickUpgradeCost = 100f;
+    public float clickUpgradeLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        insight = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        insight += insightPerSec * Time.deltaTime;
+
+        insightText.text = "i: " + insight.ToString("F1");
+        insightPerSecText.text = Math.Round(insightPerSec, 2) + " per second";
+        autoClickText.text = "Summon Familiar \ni: " + autoClickCost.ToString("F1") + "\n" + autoClickUpgradeLevel;
+        clickUpgradeText.text = "Improve da Orb \ni: " + clickUpgradeCost.ToString("F1") +"\n" + clickUpgradeLevel;
+    }
+
+    public void Click()
+    {
+        insight += insightAmount;
+    }
+
+    public void AutoClick()
+    {
+        if (insight >= autoClickCost) 
+        {
+        autoClickUpgradeLevel++;
+        insightPerSec += 0.1f;
+        insight -= autoClickCost;
+        autoClickCost *= 1.07f;
+        
+        }
+    }
+    
+    public void DoubleClick()
+    {
+        if (insight >= clickUpgradeCost)
+        {
+            clickUpgradeLevel++;
+            insightAmount *= 2;
+            insight -= clickUpgradeCost;
+            clickUpgradeCost *= 1.07f;
+        }
+    }
+}
