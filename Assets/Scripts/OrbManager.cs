000f2fe671f6fc185384ce486206df5b3d655b67
Giles-MangoFarm <rhys.oscar101@gmail.com>
2024-03-03 22:04:58 +1100

    Added Materials and first Alchemy upgrade functionality

--- Assets/Scripts/OrbManager.cs
+++ Assets/Scripts/OrbManager.cs
@@ -10,6 +10,10 @@ public class OrbManager : MonoBehaviour
     public Text insightPerSecText;
     public Text autoClickText;
     public Text clickUpgradeText;
+    public Text mat1Text;
+    public Text mat2Text;
+    public Text autoClickAlchemyText;
+    public Text clickUpgradeAlchemyText;
 
     public float insight;
     public float insightAmount = 1f;
@@ -20,6 +24,14 @@ public class OrbManager : MonoBehaviour
 
     public float clickUpgradeCost = 100f;
     public float clickUpgradeLevel;
+
+    public int mat1 = 0;
+    public float mat1Cost = 150f;
+    public int mat2 = 0;
+    public float mat2Cost = 200f;
+
+    public int alchemyMat1Cost = 1;
+    public int alchemyMat2Cost = 5;
 
     // Start is called before the first frame update
     void Start()
@@ -35,7 +47,10 @@ public class OrbManager : MonoBehaviour
         insightText.text = "i: " + insight.ToString("F1");
         insightPerSecText.text = Math.Round(insightPerSec, 2) + " per second";
         autoClickText.text = "Summon Familiar \ni: " + autoClickCost.ToString("F1") + "\n" + autoClickUpgradeLevel;
-        clickUpgradeText.text = "Improve da Orb \ni: " + clickUpgradeCost.ToString("F1") +"\n" + clickUpgradeLevel;
+        clickUpgradeText.text = "Research Technique \ni: " + clickUpgradeCost.ToString("F1") +"\n" + clickUpgradeLevel;
+        mat1Text.text = "Material 1 \ni: " + mat1Cost + "\n" + mat1;
+        mat2Text.text = "Material 2 \ni: " + mat2Cost + "\n" + mat2;
+        autoClickAlchemyText.text = "Strengthen Familiar \nM1 x" + alchemyMat1Cost + " + M2 x" + alchemyMat1Cost;
     }
 
     public void Click()
@@ -62,7 +77,35 @@ public class OrbManager : MonoBehaviour
             clickUpgradeLevel++;
             insightAmount *= 2;
             insight -= clickUpgradeCost;
-            clickUpgradeCost *= 1.07f;
+            clickUpgradeCost *= 1.15f;
+        }
+    }
+
+    public void BuyMat1()
+    {
+        if (insight >= mat1Cost)
+        {
+            mat1++;
+            insight -= mat1Cost;
+        }
+    }
+
+    public void BuyMat2()
+    {
+        if (insight >= mat2Cost)
+        {
+            mat2++;
+            insight -= mat2Cost;
+        }
+    }
+
+    public void AutoClickAlchemy()
+    {
+        if (mat1 + mat2 >= alchemyMat1Cost)
+        {
+            insightPerSec += autoClickUpgradeLevel;
+            mat1 -= alchemyMat1Cost;
+            mat2 -= alchemyMat1Cost;
         }
     }
 }
